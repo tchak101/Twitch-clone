a91cce18e6e2b13214e87143ac623d7634af2b42
Tathagata Chakraborty <tatu.sonali@gmail.com>
2024-01-09 04:03:26 +0530

    Auth pages and logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
+import { Logo } from "./_components/logo";
+
 const Authlayout = ({
     children,
   }: {
     children: React.ReactNode
   }) => {
     return ( 
-        <div className="h-full flex justify-center items-center">
+        <div className="h-full flex flex-col justify-center items-center space-y-6">
+          <Logo />
             {children}
         </div>
      );
