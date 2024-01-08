622ab938ff42baae1aad3e5a8b72f328d81b22e4
Tathagata Chakraborty <tatu.sonali@gmail.com>
2024-01-08 21:09:08 +0530

    Basic infra and auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+const Authlayout = ({
+    children,
+  }: {
+    children: React.ReactNode
+  }) => {
+    return ( 
+        <div className="h-full flex justify-center items-center">
+            {children}
+        </div>
+     );
+}
+ 
+export default Authlayout;
