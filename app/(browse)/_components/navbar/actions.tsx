2b39c0ccc9aa479e196c81c58113d68af1953b3a
Tathagata Chakraborty <tatu.sonali@gmail.com>
2024-01-15 12:01:00 +0530

    sidebar

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -37,3 +37,4 @@ export const Actions = async () => {
   )
 }
 
+
---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Wrapper } from './wrapper'
+import { Toggle } from './toggle'
+
+export const Sidebar = () => {
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  )
+}
+
