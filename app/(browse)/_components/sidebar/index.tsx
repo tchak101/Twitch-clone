d686a8fa74ca3b04090e2d17dd574559a1767f11
Tathagata Chakraborty <tatu.sonali@gmail.com>
2024-01-19 12:52:56 +0530

    recommendation list and live status

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { Wrapper } from './wrapper'
 import { Toggle } from './toggle'
+import { Recommended, RecommendedSkeleton } from './recommended'
+import { getRecommended } from '@/lib/recommended-service'
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const recommended = await getRecommended();
+
   return (
     <Wrapper>
       <Toggle />
       <div className='space-y-4 pt-4 lg:pt-0'>
-        {/* <Recommended /> */}
+        <Recommended data={recommended} />
       </div>
     </Wrapper>
   )
 }
 
+
+export const SidebarSkeleton = () => {
+  return (
+    <aside className="fixed left-0 flex flex-col w-[70px] 
+    lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+      {/* <ToggleSkeleton />
+      <FollowingSkeleton /> */}
+      <RecommendedSkeleton />
+    </aside>
+  );
+};
